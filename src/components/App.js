13dcd2fb9b150615916443a2e627f700a7240617
Will Lovell <willard.lovell@icloud.com>
2022-02-07 13:00:48 -0700

    commit tree 62d1c8381800a644f2cb56143a2ed5ae0a6093cc

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
@@ -11,7 +13,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home name={user.name}  city={user.city} color={user.color} />
       <About />
     </div>
   );
---
+++ src/components/Links.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Links(props) {
+   return (
+      
+   <div id="A">
+	  <h3>Links</h3>  
+    <div>
+    <a href={props.github}>Github</a>
+    <a href={props.linkedin}>Linkedin</a>
+    </div>
+</div>
+
+    )
+  }
+
+  export default Links
+
+  
